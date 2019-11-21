8ce5fb782762b52dacb1e52921837627d666ac72
e.paromov <e.paromov@btc-s.ru>
2019-11-21 17:41:23 +0300

    auto deploy

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+var ftp = require( 'vinyl-ftp' );
+
+const globs = [
+    "vue/*"
+];
+
+function deploy () {
+
+    const conn = ftp.create({
+        host: '91.219.194.6',
+        user: 'bh64997',
+        password: 'B7XMC074T2hD476',
+        parallel: 10,
+        log: gutil.log
+    });
+    
+   
+
+    // using base = '.' will transfer everything to /public_html correctly
+    // turn off buffering in gulp.src for best performance
+
+    return gulp.src(globs, {base: '.', buffer: false})
+        .pipe(conn.dest('/yelka.wonser.ru/wp-content/themes/yelka'));
+
+};
+
+
+gulp.task('deploy', ()=>{
+    gulp.watch(globs, deploy);
+})
